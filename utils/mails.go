6c34e3fb0752f43018c46db52a16d9aad5fdaac3
akia <ch.royer15@gmail.com>
2025-04-15 01:15:40 +0200

    Update mails.go

--- utils/mails.go
+++ utils/mails.go
@@ -7,21 +7,14 @@ import (
 )
 
 func SendMail(email string, message []byte) {
-	// Infos de connexion
 	from := "onlyflick.eemi@gmail.com"
 	password := os.Getenv("GOOGLE_MDP")
 	to := email
 
-	// SMTP server configuration
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
-
-	// Message
-
-	// Auth
 	auth := smtp.PlainAuth("", from, password, smtpHost)
 
-	// Envoi
 	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, []string{to}, message)
 	if err != nil {
 		fmt.Println(err)
